0bac3b418a97d954e53fd9dc483ff4d247993839
Muhammet Bastan <mubastan@gmail.com>
2018-08-14 19:34:51 +0800

    Update DSF.py

--- DSF.py
+++ DSF.py
@@ -1,28 +1,34 @@
-# Disjoint Set data structure with union by rank
+# Disjoint Set data structure with union by rank and path compression
 
-from numpy import *
+import numpy
 
 class Node:
     def __init__(self, parent, rank = 0):
         self.parent = parent
         self.rank = rank
-        self.size = 1 
-        
+        self.size = 1
+
 
 class DSF:
     def __init__(self, numElements):
         self.numElements = numElements
         self.numSets = numElements
-        self.nodes = array([Node(i) for i in range(numElements)])
-    
-    def find(self, x):
+        self.nodes = numpy.array([Node(i) for i in range(numElements)])
+
+    def find_nopc(self, x):
         y = x
         while y != self.nodes[y].parent:
             y = self.nodes[y].parent
-        self.nodes[y].parent = y
+        self.nodes[x].parent = y
         return y
-    
-    def union(self, x, y):        
+
+    # with recursive path compression
+    def find(self, x):
+        if self.nodes[x].parent != x:
+            self.nodes[x].parent = self.find(self.nodes[x].parent)
+        return self.nodes[x].parent
+
+    def union(self, x, y):
         xr = self.find(x)        # root, set of x
         yr = self.find(y)        # root, set of y
         if(x==y or xr==yr): return
@@ -37,17 +43,17 @@ class DSF:
             if nx.rank == ny.rank:
                 ny.rank += 1
         self.numSets -= 1
-    
+
     def setSize(self, id):
         return self.nodes[id].size
-    
+
     def reset(self):
         for i in range(self.numElements):
             self.nodes[i].rank = 0
             self.nodes[i].size = 1
             self.nodes[i].parent = i
         self.numSets = self.numElements
-    
+
     def printSet(self):
         print '\nnum elements: ', self.numElements
         print 'num sets: ', self.numSets
@@ -60,7 +66,8 @@ class DSF:
         print '\nRank: '
         for i in range(self.numElements):
             print self.nodes[i].rank,
-        
+        print
+
 if __name__ == "__main__":
     dsf = DSF(10)
     print 'dsf.numElements: ', dsf.numElements
@@ -76,12 +83,16 @@ if __name__ == "__main__":
     print 'find 3: ', dsf.find(3)
     print 'find 5: ', dsf.find(5)
     print 'num sets:', dsf.numSets
-    
+
     dsf.printSet()
     dsf.union(9,4)
     dsf.union(0,1)
-    dsf.printSet()
     dsf.union(0,5)
+
+    for e in range(dsf.numElements):
+        print('Find', e, dsf.find(e))
+
     dsf.printSet()
+
     dsf.reset()
     dsf.printSet()
